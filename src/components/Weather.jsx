afd8bc0b7e82f2199b069b84c1f2579002c63171
ReicaD <dbamanya447@gmail.com>
2025-02-10 23:02:29 -0500

    modified with weather icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,13 @@
  import React from 'react';
 import "./Weather.css";
 import search_icon from "../assets/search.png"
+import clear_icon from "../assets/clear.png";
+import cloudy_icon from "../assets/cloudy.png"
+import drizzle_icon from "../assets/drizzle.png"
+import rain_icon from "../assets/rain.png"
+import snow_icon from "../assets/snow.png"
+import weathernews_icon from "../assets/weathernews.png"
+import sun_icon from "../assets/sun.png"
  
 const Weather = () => {
     return (
@@ -9,6 +16,10 @@ const Weather = () => {
            <input type="text" placeholder='Search' />
            <img src={search_icon} alt=""  />
              </div> 
+             <img src = {clear_icon} alt="" className = "weather-icon"/>
+
+             <p>20°c</p>
+             <p>Kawempe</p>
       </div>
     );
   };
