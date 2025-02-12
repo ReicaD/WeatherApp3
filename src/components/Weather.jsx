678d95cd42c38e838eda214835ad5222d5d72924
ReicaD <dbamanya447@gmail.com>
2025-02-12 01:32:11 -0500

    modified with weather data API

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -11,29 +11,47 @@ import weathernews_icon from "../assets/weathernews.png";
 import sun_icon from "../assets/sun.png";
 
 const Weather = () => {
+   const search = async (city)=>{
+     try {
+       const url = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid=${import.meta.env.VITE_APP_ID}`;
+
+       const response = await fetch(url);
+       const data = await response.json();
+       console.log(data);
+     } catch (error) {
+       
+     }
+   }
   return (
     <div className="weather">
+      {/* Search Bar */}
       <div className="search-bar">
         <input type="text" placeholder="Search" />
-        <img src={search_icon} alt="" />
+        <img src={search_icon} alt="Search Icon" />
       </div>
-      <img src={clear_icon} alt="" className="weather-icon" />
 
+      {/* Weather Icon */}
+      <img src={clear_icon} alt="Weather Icon" className="weather-icon" />
+
+      {/* Temperature & Location */}
       <p className="temperature">20°c</p>
       <p className="location">Kawempe</p>
-      <div className="weather-data"></div>
-      <div className="col">
-        <img src={cloudy_icon} alt="" />
-        <div>
-          <p>91%</p>
-          <span>Humidity</span>
+
+      {/* Weather Data - Now Wrapping .col */}
+      <div className="weather-data">
+        <div className="col">
+          <img src={cloudy_icon} alt="Cloudy Icon" />
+          <div>
+            <p>91%</p>
+            <span>Humidity</span>
+          </div>
         </div>
-      </div>
-      <div className="col">
-        <img src={wind_icon} alt="" />
-        <div>
-          <p>3.6 km</p>
-          <span>Wind Speed</span>
+        <div className="col">
+          <img src={wind_icon} alt="Wind Icon" />
+          <div>
+            <p>3.6 km</p>
+            <span>Wind Speed</span>
+          </div>
         </div>
       </div>
     </div>
