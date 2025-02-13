2e60beca21218944031bfc3dd8a810c5b2b2fa77
ReicaD <dbamanya447@gmail.com>
2025-02-13 14:22:05 -0500

    rain

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,7 +5,7 @@ import clear_icon from "../assets/clear.png";
 import cloudy_icon from "../assets/cloudy.png";
 import wind_icon from "../assets/wind.png";
 import drizzle_icon from "../assets/drizzle.png";
-import rain_icon from "/src/assets/rain.png";
+import rain_icon from "../assets/rain.png"
 import snow_icon from "../assets/snow.png";
 import weathernews_icon from "../assets/weathernews.png";
 import sun_icon from "../assets/sun.png";
@@ -20,7 +20,7 @@ const Weather = () => {
     "01n": clear_icon,
     "02d": cloudy_icon,
     "04d": drizzle_icon,
-    "10n": rain_icon,
+    "09d": rain_icon,
     "13n": snow_icon,
     "13d": weathernews_icon,
   };
@@ -117,7 +117,7 @@ const Weather = () => {
         </>
       ) : (
         <></>
-      )}
+      )};
     </div>
   );
 };
